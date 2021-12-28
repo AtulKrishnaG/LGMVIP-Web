63227711a8da99c42234203e39f31c1f27758931
Atul Krishna Gupta <78553149+AtulKrishnaG@users.noreply.github.com>
2021-12-28 23:48:12 +0530

    OK

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings: [ "a Content Writer." ,"a Web Developer.","a UI/UX Designer.","a Btech Student."],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true
+});
